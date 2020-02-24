ee626985517c70a81f4d6f219adfa2cfdd5689d4
Mohammad Shakeri <programmer.mmdsh@gmail.com>
2020-02-24 19:20:28 +0330

    add news

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PagesComponent } from './pages/pages/pages.component';
+import { NewsComponent } from './pages/news/news.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'pages',
+    component: PagesComponent
+  },
+  {
+    path: 'news',
+    component: NewsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
